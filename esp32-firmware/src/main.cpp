6effc955f10e8dd5811e411ac29650f39d378ab4
HeyJokke <Jokke.k992@gmail.com>
2026-02-14 12:53:30 +0100

    First commit

---
+++ esp32-firmware/src/main.cpp
@@ -0,0 +1,182 @@
+#include "Arduino.h"
+#include "WiFi.h"
+#include "WebServer.h"
+#include "secrets.h"
+#include "cstring"
+#include "HTTPClient.h"
+
+WebServer server(80);
+
+int LED = 27;
+int tempPin = 34;
+int photosensPin = 35;
+
+unsigned long uptime;
+char ledStatus[8] = "";
+const char* PI_TELEMETRY_URL = "http://192.168.0.63:3000/api/telemetry";
+int tempStatus;
+int photosens;
+
+void handleRoot() {
+  server.send(200, "text/plain", "Hello from ESP32!");
+}
+
+// LED handlers
+void handleLedOff() {
+  digitalWrite(LED, LOW);
+  strncpy(ledStatus, "OFF", sizeof(ledStatus));
+  server.send(200, "application/json", "{\"LED\": \"OFF\"}");
+}
+
+void handleLedOn() {
+  digitalWrite(LED, HIGH);
+  strncpy(ledStatus, "ON", sizeof(ledStatus));
+  server.send(200, "application/json", "{\"LED\": \"ON\"}");
+}
+
+void handleLedBlink() {
+  digitalWrite(LED, LOW);
+  delay(200);
+  digitalWrite(LED, HIGH);
+  delay(200);
+  digitalWrite(LED, LOW);
+  delay(200);
+  digitalWrite(LED, HIGH);
+  delay(200);
+  digitalWrite(LED, LOW);
+  delay(200);
+  digitalWrite(LED, HIGH);
+  delay(200);
+  digitalWrite(LED, LOW);
+
+  server.send(200, "application/json", "{\"LED\": \"BLINKED\"}");
+}
+
+// Sensor handlers
+float readTemp() {
+  int raw = analogRead(tempPin);
+  float temperature = ( raw / 4095.0 ) * 100.0;
+
+  return temperature;
+}
+
+void handleTemp() {
+  float temperature = readTemp();
+
+  String jsonTemp = "{\"Temperature\":" + String(temperature) + "}";
+  server.send(200, "application/json", jsonTemp);
+}
+
+int readPhotosens() {
+  int raw = analogRead(photosensPin);
+
+  return raw;
+}
+
+void handlePhotosensitive() {
+  int photosens = readPhotosens();
+
+  String jsonPhotosens = "{\"Photosensitivity\": " + String(photosens) + "}";
+  server.send(200, "application/json", jsonPhotosens);
+}
+
+void handleStatus() {
+  tempStatus = readTemp() * 100;
+  photosens = readPhotosens();
+  uptime = millis();
+
+  String jsonLedStatus = "\"LED\": \"" + String(ledStatus) + "\"";
+  String jsonTempStatus = "\"Temperature\": " + String(tempStatus);
+  String jsonPhotosens = "\"Photosensitivity\": " + String(photosens);
+  String jsonUptimeStatus = "\"Uptime\": " + String(uptime);
+
+  String jsonDeviceStatus = "{";
+  jsonDeviceStatus += jsonLedStatus + ",";
+  jsonDeviceStatus += jsonTempStatus + ",";
+  jsonDeviceStatus += jsonPhotosens + ",";
+  jsonDeviceStatus += jsonUptimeStatus;
+  jsonDeviceStatus += "}";
+
+  server.send(200, "application/json", jsonDeviceStatus);
+}
+
+// HTTP Client
+void postTest() {
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println("postTest: No internet connection yet.");
+    return;
+  };
+
+  HTTPClient http;
+  Serial.println("postTest: HTTP client has been created");
+
+  http.begin(PI_TELEMETRY_URL);
+  http.addHeader("Content-Type", "application/json");
+
+  const char* body = "{\"temp\": 260}";
+
+  Serial.println("postTest: Sending POST request...");
+  int code = http.POST(body);
+
+  Serial.print("postTest: Pi HTTP code = ");
+  Serial.println(code);
+
+  String response = http.getString();
+  Serial.print("postTest: Response = ");
+  Serial.print(response);
+
+  http.end();
+}
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+  Serial.println("=== SETUP STARTED ===");
+  // Analog read resolution 0-4095
+  analogReadResolution(12);
+
+  // Pinmode & initialize LED
+  pinMode(LED, OUTPUT);
+  strncpy(ledStatus, "OFF", sizeof(ledStatus));
+  digitalWrite(LED, LOW);
+
+  // Set up WiFi connection
+  Serial.print("Connecting to ");
+  Serial.print(WIFI_SSID);
+  Serial.print("...");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(200);
+    Serial.print(".");
+  }
+
+  Serial.print("\nYou are connected to ");
+  Serial.println(WIFI_SSID);
+
+  // Set up endpoints
+  server.on("/", handleRoot);
+  // Status endpoint
+  server.on("/status", handleStatus);
+  // LED endpoints
+  server.on("/led/on", handleLedOn);
+  server.on("/led/off", handleLedOff);
+  server.on("/led/blink", handleLedBlink);
+  // Sensor endpoints
+  server.on("/sensor/temp", handleTemp);
+  server.on("/sensor/photosens", handlePhotosensitive);
+
+  // Start server
+  server.begin();
+  Serial.print("\nHTTP server started on ");
+  Serial.println(WiFi.localIP());
+
+  // Set up HTTP client
+  postTest();
+}
+
+
+
+void loop() {
+  server.handleClient();  
+}
---
+++ pi-backend/index.js
@@ -0,0 +1,48 @@
+import http from 'http';
+
+const PORT = 3000;
+
+let latestTelemetry = null;
+
+const server = http.createServer((req, res) => {
+	// 1. GET request
+	if (req.method === 'GET' && req.url === '/api/telemetry/latest') {
+		res.writeHead(200, { 'Content-Type': 'application/json' });
+		res.end(JSON.stringify(latestTelemetry ?? { error: 'No data yet' }));
+
+		return;
+	};
+
+	// 2. POST request TEST
+	if (req.method === 'POST' && req.url === '/api/telemetry') {
+		let body = '';
+
+		req.on('data', (chunk) => {
+			body += chunk;
+		});
+
+		req.on('end', () => {
+			try {
+				const parsed = JSON.parse(body);
+
+				latestTelemetry = parsed;
+
+				res.writeHead(200, { 'Content-Type': 'application/json' });
+				res.end(JSON.stringify({ 'ok': true, 'error': null }));
+			} catch(err) {
+				res.writeHead(400, { 'Content-Type': 'application/json' });
+				res.end(JSON.stringify({ 'ok': false, 'error': 'Invalid JSON' }));
+			};
+		});
+
+		return;
+	};
+
+	// 3. Fallback
+	res.writeHead(404, { 'Content-Type': 'application/json' });
+	res.end(JSON.stringify({ 'error': '404 not found' }));
+});
+
+server.listen(PORT, '0.0.0.0', () => {
+	console.log(`Server running smoofly at http://localhost:${PORT}/`);
+});
