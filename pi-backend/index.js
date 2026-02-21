853ec8ce1446936755af9514be2266f8f5bd6962
HeyJokke <Jokke.k992@gmail.com>
2026-02-21 21:58:50 +0100

    Created /dashboard and cors policy

--- pi-backend/index.js
+++ pi-backend/index.js
@@ -1,7 +1,12 @@
 import express from 'express'
 import db from './db.js'
+import cors from 'cors'
 
 const app = express()
+
+app.use(cors({
+	origin: 'http://localhost:5173'
+}))
 
 app.use(express.json())
 
