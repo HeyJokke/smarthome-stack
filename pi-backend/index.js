f77242516c39e4deafd0364887477306280b6277
HeyJokke <Jokke.k992@gmail.com>
2026-02-26 11:29:50 +0100

    Added utils fetchWithRetry & updated index.js

--- pi-backend/index.js
+++ pi-backend/index.js
@@ -3,19 +3,29 @@ import db from './db.js'
 import path from "path";
 import { fileURLToPath } from "url";
 import fs from "fs";
+import { fetchWithRetry } from './utils/fetchWithRetry.js';
 
 // ESM __dirname
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express()
+app.use(express.json())
+
 const DEVICES = {
 	livingroom: { baseUrl: "http://192.168.0.70" },
-	livingroom: { baseUrl: "http://192.168.0.71" },
-	livingroom: { baseUrl: "http://192.168.0.72" }
+	kitchen: { baseUrl: "http://192.168.0.71" },
+	office: { baseUrl: "http://192.168.0.72" }
 }
 
-app.use(express.json())
+function getDeviceOr404(id, res) {
+	const device = DEVICES[id]
+	if (!device) {
+		res.status(404).json({ error: `ERROR 404: Device: ${id} not found` })
+		return null
+	}
+	return device
+}
 
 app.all(/^\/esp32(\/.*)?$/, async (req, res) => {
   try {
@@ -39,7 +49,28 @@ app.all(/^\/esp32(\/.*)?$/, async (req, res) => {
   }
 });
 
-// GET Endpoint for latest telemetry
+// ----------------GET Endpoints----------------
+// Status from all devices
+app.get('/api/devices/:id/status', (req, res) => {
+	const device = getDeviceOr404(req.params.id, res)
+	if (!device) return
+
+	try {
+		const res = fetchWithRetry(`${device.baseUrl}/status`)
+
+		if (!res.ok) throw new Error(`HTTP error: ${res.status}`)
+
+		const data = res.json()
+
+		console.log(data)
+
+	} catch(err) {
+		console.error(`ERROR: ${req.params.id} connection failed: ${err.message}`)
+		res.status(502).json({ error: err.message })
+	}
+})
+
+// Latest telemetry
 app.get('/telemetry/latest', (req, res) => {
 	db.get(
 		'SELECT * FROM telemetry ORDER BY timestamp DESC LIMIT 1',
@@ -59,7 +90,7 @@ app.get('/telemetry/latest', (req, res) => {
 	)
 })
 
-// GET Endpoint for telemetry with limit
+// Telemetry entries with limit
 app.get('/telemetry', (req, res) => {
 	let limit = Number(req.query.limit ?? 0)
 
@@ -93,7 +124,8 @@ app.get('/telemetry', (req, res) => {
 	)
 })
 
-// POST Endpoint for telemetry
+// ----------------POST Endpoints----------------
+// Telemetry
 app.post('/telemetry', (req, res) => {
 	const {machine_id, temperature, photo_sens, uptime_ms} = req.body
 	const timestamp_iso = new Date().toISOString()
@@ -156,8 +188,6 @@ app.post('/telemetry', (req, res) => {
 
 // Serve dashboard build
 const distPath = path.join(__dirname, "../dashboard/dist");
-console.log("[static] distPath =", distPath, "exists =", fs.existsSync(distPath));
-
 app.use(express.static(distPath));
 
 app.listen(3000, () => {
